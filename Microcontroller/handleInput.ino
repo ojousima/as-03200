9ee1e975082d45ba7fe8f911d8adcf083ec89f7b
Garlandi <johan.holmberg@gmail.com>
2014-05-05 00:45:54 +0300

    SW: User can specifically print debug data.

--- Microcontroller/handleInput.ino
+++ Microcontroller/handleInput.ino
@@ -5,6 +5,7 @@
 *************
  * a    - adc conversion - start
  * 0-7  - Set comparator duty cycle
+ * w    - Write configuration changes to monitor
  *
 *************
 ** UPPER CASE
@@ -19,23 +20,33 @@
 void handleInput(char user_input)
 {
   if(__DEBUG__) {
-    Serial.println("\n--- GOT USER INPUT ---\n");
+    Serial.println("\n--- GOT USER INPUT ---");
   }
   
   if( (user_input >= '0') && (user_input <= 7) ) {
     MON_setComparatorDutyCycle((unsigned char) user_input);
   }
+  
   else if(user_input == 'a') {
     SPI_sendCommandToMonitor(MON_CMD_START_ADC_CONVERSION_ALL);
   }
+  else if(user_input == 'w') {
+    SPI_writeConfigurationRegister();
+  }
+  
+  
+  
   else if(user_input == 'C') {
-    SPI_readConfigurationRegister();
+    MON_printConfigurationRegister();
   }
   else if(user_input == 'D') {
-    SPI_readDiagnostics();
+    MON_printDiagnosticsRegister();
+  }
+  else if(user_input == 'L') {
+    MON_printConfigurationRegisterLocal();
   }
   else if(user_input == 'V') {
-    SPI_readAllVoltages();
+    MON_printAllVoltages();
   }
 }
 

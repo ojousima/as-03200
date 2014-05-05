732b1f3f78735e830d5835455711cc658dc25d10
Johan Holmberg <johan.holmberg@gmail.com>
2014-05-05 10:19:34 +0300

    SW: Bug fix. Improved debug messages.
    - Fixed bug in setting comparator duty cycle.
    - Improved debug when sending commands to microcontroller.

--- Microcontroller/handleInput.ino
+++ Microcontroller/handleInput.ino
@@ -19,33 +19,50 @@
 
 void handleInput(char user_input)
 {
-  if(__DEBUG__) {
-    Serial.println("\n--- GOT USER INPUT ---");
-  }
-  
-  if( (user_input >= '0') && (user_input <= 7) ) {
-    MON_setComparatorDutyCycle((unsigned char) user_input);
+  if( (user_input >= '0') && (user_input <= '7') ) {
+    MON_setComparatorDutyCycle((unsigned char) (user_input & 0x0F));
+    if(__DEBUG__) {
+      Serial.println("Changed comparatpr duty cycle in local buffer.");
+    }
   }
   
   else if(user_input == 'a') {
     SPI_sendCommandToMonitor(MON_CMD_START_ADC_CONVERSION_ALL);
+    if(__DEBUG__) {
+      Serial.println("Start adc conversion command sent to monitor.");
+    }
   }
   else if(user_input == 'w') {
     SPI_writeConfigurationRegister();
+    if(__DEBUG__) {
+      Serial.println("Wrote local configuration register buffer to monitor.");
+    }
   }
   
   
   
   else if(user_input == 'C') {
+    if(__DEBUG__) {
+      Serial.println("Print monitor configuration register.");
+    }
     MON_printConfigurationRegister();
   }
   else if(user_input == 'D') {
+    if(__DEBUG__) {
+      Serial.println("Print monitor diagnostics register.");
+    }
     MON_printDiagnosticsRegister();
   }
   else if(user_input == 'L') {
+    if(__DEBUG__) {
+      Serial.println("Print local configuration register buffer.");
+    }
     MON_printConfigurationRegisterLocal();
   }
   else if(user_input == 'V') {
+    if(__DEBUG__) {
+      Serial.println("Print all voltages.");
+    }
     MON_printAllVoltages();
   }
 }
